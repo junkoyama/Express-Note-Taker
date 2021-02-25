5f6b33b6ca768f3527ac3846e949047fc6114481
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-02-24 21:18:12 -0500

    Created server.js file and added the starter code to set up dependencies and start the PORT listener

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+// Code below is reference from Week 11 activities
+
+// Set up the dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Set up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Set up the Express app to handle data parsing
+// =============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// =============================================================
+app.use(express.static("public"));
+
+// Server starts below from/on the PORT listed above
+// =============================================================
+app.listen(PORT, () => 
+console.log(`Listening on PORT: ${PORT}`)
+);
