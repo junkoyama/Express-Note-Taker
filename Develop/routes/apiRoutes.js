2d3f41810e58ef9950fc467573506f635b994b9d
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-02-24 21:50:44 -0500

    Created routes folder and started code for apiRoutes.js. Added dependencies and setting up the routing for get post delete requests

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,18 @@
+// Code below is using code reference from Week 11 activities
+
+// Set up dependencies
+const fs = require('fs');
+const path = require('path');
+const dbNotes = require('../db/db.json');
+
+module.exports = (app) => {
+
+    //app.get
+    //=================================
+
+    //app.post
+    //=================================
+
+    //app.delete
+    //=================================
+};
